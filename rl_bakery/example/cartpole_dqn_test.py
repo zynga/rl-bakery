28fe455034a053282eaf8b12a44c1fcaec7645f0
Mehdi Ben Ayed <mbenayed@zynga.com>
2020-06-18 10:46:04 -0400

    updated dqn unit test params

--- rl_bakery/example/cartpole_dqn_test.py
+++ rl_bakery/example/cartpole_dqn_test.py
@@ -9,7 +9,8 @@ from unittest import TestCase
 class TrainEval(TestCase):
     def testDQNCartPole(self):
 
-        cartpole = ExampleCartPole(tb_path="/tmp/rl_application/dqn_cartpole_unit_test")
+        cartpole = ExampleCartPole(tb_path="/tmp/rl_application/dqn_cartpole_unit_test",
+                                   learning_rate=1e-4, mini_batch_size=128, num_iterations=5000)
         avg_rwd = cartpole.run()
         eval_runs = 3
         self.assertGreater(sum(avg_rwd[-eval_runs:])/eval_runs, 185)
