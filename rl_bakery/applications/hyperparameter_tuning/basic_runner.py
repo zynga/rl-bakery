1d2ba41a14f46aa3e74e7d270b7b3fdb6df7c37f
Nim Wijetunga <nim.wijetunga@gmail.com>
2020-08-19 17:37:32 -0400

    add run manager

---
+++ rl_bakery/applications/hyperparameter_tuning/basic_runner.py
@@ -0,0 +1,28 @@
+from hyperopt import hp
+from rl_bakery.applications.hyperparameter_tuning.run_manager import RunManager
+
+class BasicRunner(RunManager):
+
+    def __init__(self):
+        space = {
+            'a': hp.choice('a',
+            [
+                ('case 1', 1 + hp.lognormal('c1', 0, 1)),
+                ('case 2', hp.uniform('c2', -10, 10))
+            ])
+        }
+        super(BasicRunner, self).__init__(None, None, {}, space)
+    
+    @staticmethod
+    def _eval_fn(run_manager, params):
+        case, val = params['a']
+        if case == 'case 1':
+            return val
+        else:
+            return val ** 2
+
+if __name__ == '__main__':
+    basic_runner = BasicRunner()
+    exp_id = basic_runner.create_experiment('test_exp_1')
+    best = basic_runner.run(exp_id, 1, 1)
+    print(best)
---
+++ rl_bakery/applications/hyperparameter_tuning/run_manager.py
@@ -0,0 +1,59 @@
+import mlflow
+from hyperopt import fmin, tpe, hp, Trials, STATUS_OK
+from hyperopt import SparkTrials
+from functools import partial
+
+class RunManager(object):
+
+    def __init__(self, simulator, agent, simulator_config, hyperparameter_config):
+        """
+            :simulator: should be a PyEnviroment/ OpenAI Gym Enviroment
+            :agent: should be a wrapper around the tf_agent.TFAgent class
+            :simulator_config: any parameters needed for the simulator
+            :hyperparameter_config: a python dictionary of hyperparams representing a hyperopt search space
+        """
+        self._sim = simulator
+        self._agent = agent
+        self._sim_conf = simulator_config
+        self._hyper_conf = hyperparameter_config
+        self._experiment_list = set()
+    
+    def create_experiment(self, experiment_name, artifact_location=None):
+        """
+            :experiment_name: name of the mlflow experiment (should be unique across all other experiments)
+            :artifact_location: where to store artifacts, if None then a default location is chose
+            
+            :returns: the mlflow experiment ID 
+        """
+        exp_id = mlflow.create_experiment(experiment_name, artifact_location=artifact_location)
+        self._experiment_list.add(exp_id)
+        return exp_id
+    
+    @staticmethod
+    def _eval_fn(self, params):
+        """
+            :params: a dict containing chosen hyperparameters (by hyperopt) to be used for a single run
+
+            :returns: a float which is used to compare different runs
+        """
+        raise Exception('Implement Eval Method')
+
+    def run(self, experiment_id, max_evals, parallelism, algo=tpe.suggest):
+        """
+            :experiment_id: The experiment id to be run (should be a result from self.create_experiment)
+            :max_evals: the number of hyperopt trials to run (must be > 0)
+            :parallelism: how many trials to run in parallel (must be > 0)
+            :algo: Algorithm used by hyperOpt to search in the hyper param space
+
+            :returns: the best hyperparameter combination
+        """
+
+        spark_trials = SparkTrials(parallelism=parallelism)
+        
+        with mlflow.start_run(experiment_id=experiment_id):
+            return fmin(fn=partial(self._eval_fn, self),
+                        space=self._hyper_conf,
+                        return_argmin=True,
+                        trials=spark_trials,
+                        algo=algo,
+                        max_evals=max_evals)
